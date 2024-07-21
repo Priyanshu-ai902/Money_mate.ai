ed1769b6998f9c3293b47d43086a84485f3235d3
Priyanshu-ai902 <jhapriyanshus111@gmail.com>
2024-07-21 23:51:43 +0530

    initial setup

---
+++ src/app/_components/Header.tsx
@@ -0,0 +1,15 @@
+"use client"
+
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+
+function Header() {
+  return (
+    <div>
+      <Button/>
+    </div>
+  )
+}
+
+export default Header
