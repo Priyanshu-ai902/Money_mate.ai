7216a87cf66c76bb996b4c93dfbab3bcd92f5540
Priyanshu-ai902 <jhapriyanshus111@gmail.com>
2024-07-22 23:45:00 +0530

    header craeted

--- src/app/_components/Header.tsx
+++ src/app/_components/Header.tsx
@@ -1,13 +1,21 @@
 "use client"
 
 import { Button } from '@/components/ui/button'
+import { useUser } from '@clerk/nextjs'
 import React from 'react'
 
 
 function Header() {
+  const {user, isSignedIn } = useUser();
   return (
-    <div>
-      <Button/>
+    <div className='p-5 flex justify-between items-center border shadow-sm'>
+      <div className="flex flex-row items-center">
+        <span>Money-mateAi</span>
+      </div>
+      <div className="">
+        <button>button 1</button>
+        <button>button 2</button>
+      </div>
     </div>
   )
 }
