3d478a2a1d1b9fc0e1331dd3ee36b181b3a6bc2b
Priyanshu-ai902 <jhapriyanshus111@gmail.com>
2025-01-06 23:35:29 +0530

    add transaction is add

--- app/(main)/transaction/create/page.jsx
+++ app/(main)/transaction/create/page.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import { getUserAccounts } from "@/actions/dashboard";
+import { defaultCategories } from "@/data/categories";
+import { getTransaction } from "@/actions/transaction";
+import { AddTransactionForm } from "./_components/transaction-form";
 
-const page = () => {
+export default async function AddTransactionPage({ searchParams }) {
+  const accounts = await getUserAccounts();
+  const editId = searchParams?.edit;
+
+  let initialData = null;
+  if (editId) {
+    const transaction = await getTransaction(editId);
+    initialData = transaction;
+  }
+
   return (
-    <div>
-      
+    <div className="px-5 bg-black pt-12 ">
+      <div className="flex justify-center md:justify-normal mb-12 ">
+        <h1 className="mx-auto text-5xl text-teal-400">Add Transaction</h1>
+      </div>
+      <AddTransactionForm
+        accounts={accounts}
+        categories={defaultCategories}
+        editMode={!!editId}
+        initialData={initialData}
+      />
     </div>
-  )
+  );
 }
-
-export default page
