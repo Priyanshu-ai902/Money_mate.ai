79d88c850ca103a1aa413c7d7b4611c35189e096
Priyanshu-ai902 <jhapriyanshus111@gmail.com>
2025-01-07 22:09:28 +0530

    changes in email receive from inngest

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,15 +1,13 @@
 import { serve } from "inngest/next";
 
 import { inngest } from "@/lib/inngest/client";
-import {
-  checkBudgetAlerts,
 
-} from "@/lib/inngest/functions";
+import { checkBudgetAlert } from "@/lib/inngest/functions";
 
 export const { GET, POST, PUT } = serve({
   client: inngest,
   functions: [
+    checkBudgetAlert,
     
-    checkBudgetAlerts,
   ],
 });
